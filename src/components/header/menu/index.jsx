b21ffec561299eaebf047ac8d95072d834a2084e
Fernanda Freitas <idea.fernanda@gmail.com>
2023-08-03 19:02:18 +0100

    Change color on close button

--- src/components/header/menu/index.jsx
+++ src/components/header/menu/index.jsx
@@ -66,8 +66,8 @@ export default function Menu({ handleClose }) {
           xmlns="http://www.w3.org/2000/svg"
           className={styles.button}
         >
-          <path d="M1.5 1.5L67 67" stroke="white" />
-          <path d="M66.5 1L0.999997 66.5" stroke="white" />
+          <path d="M1.5 1.5L67 67" stroke="#ffcece" />
+          <path d="M66.5 1L0.999997 66.5" stroke="#ffcece" />
         </motion.svg>
       </div>
 
